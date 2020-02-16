d4384c9ed708fe176679f34b5d98e1f167cf0e25
William Pratt <will@Williams-MBP.home>
2020-02-16 20:22:26 +0000

    parsing scene on command line

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import argparse
 from home import Home
 
-state = "lounge_only"
+parser = argparse.ArgumentParser(description='Change the lights.')
+parser.add_argument('Scene', nargs=1,
+                    help='The name of the light scene to switch to')
+
+args = parser.parse_args()
+
+state = args.Scene[0]
 house = Home()
 house.set_state(state)
